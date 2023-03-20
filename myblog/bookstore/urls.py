c95a0cb68cdd841ac146d5662ec4f44bdd8a9b4d
pravallikayenumala <128349296+pravallikayenumala@users.noreply.github.com>
2023-03-20 11:28:59 +0530

    Add files via upload

---
+++ myblog/bookstore/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from .views import BookViewSet
+
+router = routers.DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
